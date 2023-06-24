7abb7889f9f2ce56d58e125b59b64310dfd7b894
Wolf_of_Windows <126786628+Yekuuun@users.noreply.github.com>
2023-06-24 19:06:38 +0200

    [UPDATE] Adding docker

---
+++ MicroServices/OtherService/Migrations/20230624170539_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OtherService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    user_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    user_username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    user_email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    user_passwordhash = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    user_passwordsalt = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    user_name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    user_surname = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.user_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
